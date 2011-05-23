b19afea054514b92c6bdb6169d16ee7d3ed85df2
Fabiano Fidêncio <fidencio@morphine.(none)>
2011-05-23 16:54:26 -0300

    Populacao ordenada por makespan

--- src/population.py
+++ src/population.py
@@ -5,13 +5,14 @@ import random
 from fitness import fitness
 from flowshop import Problem, read_problem
 from globals_settings import PROBLEM_FILE
+from operator import itemgetter, attrgetter
 
 class Individual:
     def __init__(self, sequence, makespan):
         self.sequence = sequence
         self.makespan = makespan
-    def show(self):
-        return (self.sequence, self.makespan)
+    def __repr__(self):
+        return repr((self.sequence, self.makespan))
 
 #XXX: mover a função para a classe Individual
 def create_individual(Problem):
@@ -21,29 +22,23 @@ def create_individual(Problem):
     makespan = fitness(sequence, Problem)
     return (sequence, makespan)
 
-#XXX: ordenar população por ordem de makespan
-class Population:
-    def __init__(self, size):
-        self.size = size
-        self.population = []
-    def create_population(self, Problem):
-        i = 0
-        while i < self.size:
-            sequence, makespan = create_individual(Problem)
-            I = Individual(sequence, makespan)
-            self.population.append(I)
-            i = i + 1
-    def get_population(self):
-        return self.population
-        
+def create_population(size, Problem):
+    i = 0
+    population = []
+    while i < size:
+        sequence, makespan = create_individual(Problem)
+        I = Individual(sequence, makespan)
+        population.append(I)
+        i = i + 1
+    return sorted(population, key=lambda individual: individual.makespan)
+
 # testes
 P = read_problem(PROBLEM_FILE)
 
 #print create_individual(P)
 
-pop = Population(10)
-pop.create_population(P)
-#print pop.get_population()
+pop = create_population(10, P)
+print pop
 
 
 
